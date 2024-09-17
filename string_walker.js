67f61d9baed1bc8f3546e5fe23148018e2756389
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2024-09-17 21:11:40 +0200

    remove legacy

--- string_walker.js
+++ string_walker.js
@@ -8,94 +8,6 @@ let rateKnob = new ContinuousDial(rateKnobDiv, 0, 100, -120, 120, 50)
 let depthKnob = new ContinuousDial(depthKnobDiv, 0, 100, -120, 120, 50)
 let modeKnob = new FixedDial(modeKnobDiv, [-78, -106, -137, -167, 164, 134, 103], 3)
 
-const C2   = 0
-const Csh2 = 1
-const D2   = 2
-const Dsh2 = 3
-const E2   = 4 // E string
-const F2   = 5
-const Fsh2 = 6
-const G2   = 7
-const Gsh2 = 8
-const A2   = 9 // A string
-const Ash2 = 10
-const B2   = 11
-
-const C3   = 12
-const Csh3 = 13
-const D3   = 14 // D string
-const Dsh3 = 15
-const E3   = 16
-const F3   = 17
-const Fsh3 = 18
-const G3   = 19 // G string
-const Gsh3 = 20
-const A3   = 21
-const Ash3 = 22
-const B3   = 23 // B string
-
-const C4   = 24
-const Csh4 = 25
-const D4   = 26
-const Dsh4 = 27
-const E4   = 28 // e string
-const F4   = 29
-const Fsh4 = 30
-const G4   = 31
-const Gsh4 = 32
-const A4   = 33
-const Ash4 = 34
-const B4   = 35
-
-const C5   = 36
-const Csh5 = 37
-const D5   = 38
-const Dsh5 = 39
-const E5   = 40
-const F5   = 41
-const Fsh5 = 42
-const G5   = 43
-const Gsh5 = 44
-const A5   = 45
-const Ash5 = 46
-const B5   = 47
-
-const C6   = 48
-const Csh6 = 49
-const D6   = 50
-const Dsh6 = 51
-const E6   = 52
-const F6   = 53
-const Fsh6 = 54
-const G6   = 55
-const Gsh6 = 56
-const A6   = 57
-const Ash6 = 58
-const B6   = 59
-
-// Notes duration in terms of beats per second for 4/4
-const FOURTH = 1
-const EIGHTH = FOURTH / 2
-const SIXTEENTH = EIGHTH / 2
-const HALF = FOURTH * 2
-const WHOLE = HALF * 2
-
-const FOURTH_DOT = FOURTH * 1.5
-const EIGHTH_DOT = EIGHTH * 1.5
-const SIXTEENTH_DOT = SIXTEENTH * 1.5
-const HALF_DOT = HALF * 1.5
-
-const FOURTH_PAUSE = -FOURTH
-const EIGHTH_PAUSE = -EIGHTH
-const SIXTEENTH_PAUSE = -SIXTEENTH
-const HALF_PAUSE = -HALF
-const WHOLE_PAUSE = -WHOLE
-
-const FOURTH_DOT_PAUSE = -FOURTH_DOT
-const EIGHTH_DOT_PAUSE = -EIGHTH_DOT
-const SIXTEENTH_DOT_PAUSE = -SIXTEENTH_DOT
-const HALF_DOT_PAUSE = -HALF_DOT
-
 // Settings
 const BPM = 120
 const GUITAR_VOLUME = 1.0
@@ -110,7 +22,6 @@ let guitarEffectsChain = []
 let context
 
 let guitarSample
-let SAMPLE_NOTE
 
 let soundNodes = []
 
@@ -136,14 +47,12 @@ async function loadSample(path) {
 }
 
 async function loadGuitarSample() {
-    SAMPLE_NOTE = D3;
     return await loadSample('./sound/sample02.mp3')
 }
 
-function createGuitarSource(noteToPlay) {
+function createGuitarSource() {
     const source = context.createBufferSource()
     source.buffer = guitarSample
-    source.playbackRate.value = 2 ** ((noteToPlay - SAMPLE_NOTE) / 12)
     return source
 }
 
@@ -203,10 +112,9 @@ function clamp(val, min, max) {
     return Math.min(Math.max(val, min), max)
 }
 
-function playGuitarSound(notes, duration) {
-    const seconds = duration / (BPM / 60)
-    const startTime = context.currentTime + guitarTimeline
-    const endTime = startTime + seconds
+function playSample() {
+    const seconds = guitarSample.duration
+    const startTime = 0
 
     const lfoSampleRate = 256
     const lfoTime = lfoSampleRate * seconds
@@ -217,8 +125,8 @@ function playGuitarSound(notes, duration) {
         ? remap(depthKnob.value, 0, 100, 0, 1)
         : remap(depthKnob.value, 0, 100, 0, 60)
 
-    for (let noteIndex = 0; noteIndex < notes.length; noteIndex++) {
-        const sample = createGuitarSource(notes[noteIndex])
+    {
+        const sample = createGuitarSource()
 
         sample.connect(guitarEffectsChain[0])
 
@@ -235,50 +143,18 @@ function playGuitarSound(notes, duration) {
             }
         }
         
-        sample.start(startTime + randomFloat(0, GUITAR_PLAYING_ERRORS))
-        sample.stop(endTime + DAMPING_DURATION + randomFloat(0, GUITAR_PLAYING_ERRORS))
-
+        sample.start()
         soundNodes.push(sample)
     }
 
     if (pedalType == CHORUS) {
-        for (let noteIndex = 0; noteIndex < notes.length; noteIndex++) {
-            const sample = createGuitarSource(notes[noteIndex])
-
-            sample.connect(guitarEffectsChain[0])
-
-            sample.start(startTime + randomFloat(0, GUITAR_PLAYING_ERRORS))
-            sample.stop(endTime + DAMPING_DURATION + randomFloat(0, GUITAR_PLAYING_ERRORS))
-
-            soundNodes.push(sample)
-        }
+        const sample = createGuitarSource()
+        sample.connect(guitarEffectsChain[0])
+        sample.start()
+        soundNodes.push(sample)
     }
 
     guitarTimeline += seconds
-}
-
-function playGuitarPause(duration) {
-    const seconds = Math.abs(duration) / (BPM / 60)
-    guitarTimeline += seconds
-}
-
-SHORT = 0
-FULL = 1
-
-function createPowerChord(tonica, type = FULL) {
-    return type == SHORT
-        ? [tonica, tonica + 7]
-        : [tonica, tonica + 7, tonica + 12]
-}
-
-function getRandomInt(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-}
-
-function generateNote() {
-    return getRandomInt(E2, E3)
 }
 
 button.onclick = async () => {
@@ -291,7 +167,6 @@ button.onclick = async () => {
     }
     soundNodes = []
 
-    playGuitarSound([D3], WHOLE*100)
-
+    playSample()
     guitarTimeline = 0
 };
